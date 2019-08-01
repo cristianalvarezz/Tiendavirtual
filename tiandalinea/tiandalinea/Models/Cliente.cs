692cc9174dab9e1f0bb7e26df60a27dcb8a3bd7b
cristianalvarezz <alvarescristian46@gmail.com>
2019-08-01 18:00:24 -0500

    RRRRRRRRR

--- tiandalinea/tiandalinea/Models/Cliente.cs
+++ tiandalinea/tiandalinea/Models/Cliente.cs
@@ -7,22 +7,7 @@ namespace tiandalinea.Models
 {
     public class Cliente
     {
-        private string login;
-        private string contrasena;
-
-        public string Login { set; get; }
-        public string Contrasena { set; get; }
-
-        public Cliente(string pLogin, string pContrasena)
-        {
-            login = pLogin;
-            contrasena = pContrasena;
-        }
-        public Cliente()
-        {
-            login = "";
-            contrasena = "";
-        }
+        
 
     }
 }
