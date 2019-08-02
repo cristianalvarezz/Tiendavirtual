0f784d5bd7fb699d3b19ed3d85682a28c837e183
cristianalvarezz <alvarescristian46@gmail.com>
2019-08-01 19:59:31 -0500

    utimo commit

---
+++ tiandalinea/tiandalinea/Models/CategoriaProductos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tiandalinea.Models
+{
+    public class CategoriaProductos
+    {
+        private string categoria;
+        private string nombreProducto;
+        private string descripcion;
+        private string  imagen;
+        public string Categoria { set; get; }
+        public string NombreProducto { set; get; }
+        public string Descripcion { set; get; }
+        public string Imagen { set; get; }
+
+
+        public CategoriaProductos(string pCategoria, string pNombreProducto, string pDescripcion,string pImagen)
+        {
+            pCategoria = categoria;
+
+            pNombreProducto  = nombreProducto;
+
+            pDescripcion = descripcion;
+            pImagen = imagen;
+
+        }
+        public CategoriaProductos()
+        {
+            categoria ="";
+
+            nombreProducto = "";
+
+            descripcion = "";
+            imagen = "";
+        }
+    }
+}
